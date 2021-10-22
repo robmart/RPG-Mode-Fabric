50cdbf6a03834f46fde209574c965f4f9477e0ec
Robmart <robinmartensson01@gmail.com>
2021-10-22 06:51:18 +0200

    Effects is lib for Creatures

---
+++ RPG-Mode-Effects/src/main/java/robmart/mod/rpgmodeeffects/client/RPGModeEffectsClient.java
@@ -0,0 +1,10 @@
+package robmart.mod.rpgmodeeffects.client;
+
+import net.fabricmc.api.ClientModInitializer;
+
+public class RPGModeEffectsClient implements ClientModInitializer {
+    @Override
+    public void onInitializeClient() {
+
+    }
+}
---
+++ RPG-Mode-Effects/src/main/java/robmart/mod/rpgmodeeffects/common/RPGModeEffects.java
@@ -0,0 +1,10 @@
+package robmart.mod.rpgmodeeffects.common;
+
+import net.fabricmc.api.ModInitializer;
+
+public class RPGModeEffects implements ModInitializer {
+    @Override
+    public void onInitialize() {
+
+    }
+}
