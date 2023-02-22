b905a2ddf733d452486c05b39a232a1ed41296e4
Robmart <robmart@users.noreply.github.com>
2023-02-22 16:01:22 +0100

    Fix small errors

--- RPG-Mode-Effects/src/main/java/robmart/mod/rpgmodeeffects/common/entity/effect/RPGStatusEffects.java
+++ RPG-Mode-Effects/src/main/java/robmart/mod/rpgmodeeffects/common/entity/effect/RPGStatusEffects.java
@@ -1,9 +1,8 @@
 package robmart.mod.rpgmodeeffects.common.entity.effect;
 
-import net.minecraft.entity.effect.StatusEffect;
-import net.minecraft.entity.effect.StatusEffectCategory;
+import net.minecraft.registry.Registries;
+import net.minecraft.registry.Registry;
 import net.minecraft.util.Identifier;
-import net.minecraft.util.registry.Registry;
 import robmart.mod.rpgmodecore.common.reference.Reference;
 
 public class RPGStatusEffects {
@@ -12,8 +11,8 @@ public class RPGStatusEffects {
     public static final RPGStatusEffect SLEEP = new SleepStatusEffect();
 
     public static void register() {
-        Registry.register(Registry.STATUS_EFFECT, new Identifier(Reference.MOD_ID, "petrification"), PETRIFICATION);
-        Registry.register(Registry.STATUS_EFFECT, new Identifier(Reference.MOD_ID, "charm"), CHARM);
-        Registry.register(Registry.STATUS_EFFECT, new Identifier(Reference.MOD_ID, "sleep"), SLEEP);
+        Registry.register(Registries.STATUS_EFFECT, new Identifier(Reference.MOD_ID, "petrification"), PETRIFICATION);
+        Registry.register(Registries.STATUS_EFFECT, new Identifier(Reference.MOD_ID, "charm"), CHARM);
+        Registry.register(Registries.STATUS_EFFECT, new Identifier(Reference.MOD_ID, "sleep"), SLEEP);
     }
 }
